ef4794a1632ed2a2af3f64d5ded7726415364d0a
ohidulalam <mdnoyonctg15@gmail.com>
2022-11-01 23:36:18 +0600

    I, J and K problem solve throw

---
+++ problem-I.cpp
@@ -0,0 +1,18 @@
+// I. Welcome for you with Conditions
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int A, B;
+    cin >> A >> B;
+
+    if (A >= B)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
---
+++ problem-J.cpp
@@ -0,0 +1,19 @@
+// J. Multiples
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A, B;
+    cin >> A >> B;
+
+    if ((A % B == 0) || (B % A == 0))
+        cout << "Multiples";
+    else
+        cout << "No Multiples";
+
+    return 0;
+}
