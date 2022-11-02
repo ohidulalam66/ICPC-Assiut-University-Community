186c7ebbc0da689b97636dd0b0ebb9cc10b431df
ohidulalam <mdnoyonctg15@gmail.com>
2022-11-02 21:06:04 +0600

    L, M problem solve throw

--- Sheet#1/problem-M.cpp
+++ Sheet#1/problem-M.cpp
@@ -8,7 +8,23 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-int a,b;
+    char x;
+    cin >> x;
+
+    if (x >= '0' && x <= '9')
+        cout << "IS DIGIT"
+             << "\n";
+    else
+    {
+        cout << "ALPHA"
+             << "\n";
+        if (x >= 'A' && x <= 'Z')
+            cout << "IS CAPITAL"
+                 << "\n";
+        else
+            cout << "IS SMALL"
+                 << "\n";
+    }
 
     return 0;
 }
