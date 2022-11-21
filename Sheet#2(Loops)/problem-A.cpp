740e738820c8ac4727910e1fd3a4565ba3024bdf
ohidulalam <mdnoyonctg15@gmail.com>
2022-11-21 13:34:01 +0600

    Sheet#2 A problem solve throw

---
+++ Sheet#2(Loops)/problem-A.cpp
@@ -0,0 +1,20 @@
+// A. 1 to N
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, i;
+    cin >> N;
+
+    for (i = 1; i <= N; i++)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
