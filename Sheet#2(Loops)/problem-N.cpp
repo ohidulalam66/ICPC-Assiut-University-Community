1084ed661fc28fc9c9749d054d3df62b0bc9d6b9
ohidulalam <mdnoyonctg15@gmail.com>
2022-11-24 15:02:15 +0600

    Sheet#2 N problem resolve throw

---
+++ Sheet#2(Loops)/problem-N.cpp
@@ -0,0 +1,28 @@
+// N. Numbers Histogram
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char S;
+    cin >> S;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, i;
+        cin >> N;
+        for (i = 0; i < N; i++)
+        {
+            cout << S;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
