a6bbab3121f4f4254b71d7f2c11389c02e1b1785
ohidulalam <mdnoyonctg15@gmail.com>
2022-11-25 21:52:55 +0600

    Sheet#2 R problem resolve throw

---
+++ Sheet#2(Loops)/problem-R.cpp
@@ -0,0 +1,34 @@
+// R. Sequence of Numbers and Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while (true)
+    {
+        int n, m, i;
+        long long sum = 0;
+        cin >> n >> m;
+
+        if ((n <= 0) || m <= 0)
+            return 0;
+        else
+        {
+            if (n > m)
+                swap(n, m);
+            for (i = n; i <= m; i++)
+            {
+                cout << i << " ";
+                sum += i;
+            }
+            cout << "sum =" << sum << "\n"
+                 << "\n";
+        }
+    }
+}
+
+// Not Solve in program
